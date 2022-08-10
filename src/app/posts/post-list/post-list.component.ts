d748fc378632531df9074022bbe10f454464bd52
Chet Hill <chetdhill@gmail.com>
2022-08-10 16:43:25 -0500

    Fix CORS error

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -11,12 +11,6 @@ import { PostsService } from '../posts.service';
 })
 export class PostListComponent implements OnInit, OnDestroy {
 
-  // posts = [
-  //   { title: "First Post", content: "This is the first post content" },
-  //   { title: "Second Post", content: "This is the second post content" },
-  //   { title: "Third Post", content: "This is the third post content" }
-  // ]
-
   posts: Post[] = [];
   private postsSub!: Subscription;
 
