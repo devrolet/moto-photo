6a22de6c101e039dd255a0102d1403f6635b9c6c
Chet Hill <chetdhill@gmail.com>
2022-08-10 15:08:08 -0500

    Create Node Server

---
+++ server.js
@@ -0,0 +1,9 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.end('This is the first response');
+});
+
+const port = process.env.PORT || 3000;
+
+server.listen(port);
